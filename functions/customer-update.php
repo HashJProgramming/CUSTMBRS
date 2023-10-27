9dbf8df2c6ed50f095fbc5b5f27c8b1c8f05b77d
HashJProgramming <JoshuaMarceloAmbalong@gmail.com>
2023-10-27 18:57:16 +0800

    Version - 3.3 Customer Update Bug fixed

--- functions/customer-update.php
+++ functions/customer-update.php
@@ -7,6 +7,18 @@ try {
     $address = $_POST['address'];
     $phone = $_POST['phone'];
 
+    $sql = "SELECT * FROM customers WHERE fullname = :fullname OR phone = :phone AND id != :id";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':fullname', $fullname);
+    $stmt->bindParam(':phone', $phone);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        header('Location: ../customer.php?type=error&message='.$fullname.' or '.$phone.' is already exist');
+        exit();
+    }
+
     $sql = "UPDATE customers SET fullname = :fullname, address = :address, phone = :phone WHERE id = :id";
     $statement = $db->prepare($sql);
     $statement->bindParam(':id', $id);
